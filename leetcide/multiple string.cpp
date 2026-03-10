7e1e328894ec0db9d541c0d97688c7319c96bf56
Kemba Devi <155751029+KembaDe@users.noreply.github.com>
2026-03-10 09:21:56 +0530

    Create multiple string.cpp

---
+++ leetcide/multiple string.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string multiply(string num1, string num2) {
+    if (num1 == "0" || num2 == "0") return "0";
+
+    int n = num1.size(), m = num2.size();
+    vector<int> res(n + m, 0);
+r
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            int mul = (num1[i] - '0') * (num2[j] - '0');
+            int p1 = i + j, p2 = i + j + 1;
+            
+            int sum = mul + res[p2];
+            res[p2] = sum % 10;
+            res[p1] += sum / 10;
+        }
+    }
+
+    string result = "";
+    for (int digit : res) {
+        if (!(result.empty() && digit == 0)) {
+            result += to_string(digit);
+        }
+    }
+    return result.empty() ? "0" : result;
+}
